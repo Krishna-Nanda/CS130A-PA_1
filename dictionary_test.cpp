324bde0b56df0387d4f7b3c78b6994e84ca8498d
Krishna Nanda <krishnananda@ucsb.edu>
2019-10-27 19:31:25 -0700

    updated test file

--- dictionary_test.cpp
+++ dictionary_test.cpp
@@ -1,64 +1,44 @@
 #include "Dictionary.h"
 #include <iostream>
-#include <boost_1_71_0/boost/algorithm/string/classification.hpp>
-#include "csv.h"
-#include "boost_1_71_0/boost/algorithm/string.hpp"
-
 using namespace std;
 
 // Driver program
-//int main()
-//{
-//     //Initialize the dictionary.
-//    Dictionary dict(9);
-//
-//     //BulkInsert the keys into the dictionary.
-//    string strs[] = {"Fred Astaire", "Lauren Bacall", "Brigitte Bardot", "John Belushi", "Ingmar Bergman","Humphrey Bogart","James Cagney","Bette Davis","Henry Fonda", "Bette Davis"};
-//    int n = 9;
-//    dict.bulkInsert(n, strs);
-//    //dict.Print();
-//
-//     //Insert "Humphrey Bogart" into the dictionary.
-//    dict.insert("Humphrey Bogart");
-//
-//    // Remove "Lauren Bacall" from the dictionary.
-//    dict.remove("Lauren Bacall");
-//
-//    // Find "Fred Astaire" in the dictionary.
-//    cout << dict.find("Fred Astaire") << endl;
-//
-//    // Find "Lauren Bacall" in the dictionary.
-//    cout << dict.find("Lauren Bacall") << endl;
-//
-//    return 0;
-//}
-int main() {
-    Dictionary Dic(100);
-    io::LineReader in("name.basics.tsv");
-    vector<string> data;
-    int counter = 0;
-    int index = 0;
-    string *t = new string[100];
-    while (char *line = in.next_line()) {
-        string a = line;
-        //boost::erase_all(a, " ");
-        split(data, a, boost::is_any_of("\t"));
-        string w = data[1];
-        a += "\n";
-        t[index] = w;
-        //cout << t[index] << endl;
-        index++;
-        counter++;
-        if (counter == 100) {
-            break;
-        }
-    }
-    Dic.bulkInsert(100, t);
-    //Dic.Print();
-    Dic.insert("Sam Pettus");
-    Dic.find("Sam Pettus");
-    Dic.remove("Sam Pettus");
-    Dic.find("Sam Pettus");
-    //Dic.print();
+int main()
+{
+    // Initialize the dictionary.
+    Dictionary dict(52);
+
+    // BulkInsert the keys into the dictionary.
+    string strs[] = {"Fred AstairQe", "Lauren Bacall", "Brigitte BaVrdot", "John BeluEshi", "Ingmar BergRman", "Akeel StubbFs", "Jenna CraGig", 
+        "Ella-Grace Henderson", "Hebe Lara", "Gurpreet Woodley", "Angus Shepherd", "Violet Kline", "Caitlan Simmons", "Gred AstaiYrz", "Lauren BacaYlz", "Brigitte BYardoz", "John BeluYshz", "Ingmar BergYmaz"
+        , "Akeel Stubbz", "Jenna Craiz", 
+        "Ella-Grace Hendersoz", "Hebe Laraz", "Gurpreet Woodlez", "Angus Shepherz", "Violet Klinz", "Caitlan Simmonz",
+        "Fred Astaire", "Lauren Bacallzd", "Brigitte Barhdbdot", "John BelushFi", "Ingmar Bergmanqze", "Akeel Stubbs", "Jenna Craig", 
+        "Ella-Grace HendersonHq", "Hebe LaraDq", "Gurpreet WooQdleyq", "Angus ShephWerdq", "Violet 1Klinqe", "Caitlan Si3mmonsq", "Gred Astairzq", "Lauren Bac7alzq", "Brigitte Bardozq", "John Belushzq", "Ingmar Bergmazq"
+        , "Akeel StubbFHz", "Jenna CrFHaiz", 
+        "Ella-Grace HendersoHzq", "Hebe LarHazq", "Gurpreet WoodlezVq", "Angus Shdepherzq", "Violet Klsinqz", "Caitlan Simmsonzq"};
+
+    int n = 52;
+    dict.bulkInsert(n, strs);
+    cout << "Here 1" << endl;
+
+    // Insert "Humphrey Bogart" into the dictionary.
+    dict.insert("Humphrey Bogart");
+    cout << "Here 2" << endl;
+
+    // Remove "Lauren Bacall" from the dictionary.
+    //dict.remove("Lauren Bacall");
+    cout << "Here 3" << endl;
+    // Find "Fred Astaire" in the dictionary.
+    cout << dict.find("Fred Astaire") << endl;
+    cout << "Here 4" << endl;
+    // Find "Lauren Bacall" in the dictionary.
+    cout << dict.find("Lauren Bacall") << endl;
+    cout << "Here 5" << endl;
+    cout << dict.find("Humphrey Bogart") << endl;
+    cout << "Here 6" << endl;
+    dict.remove("Humphrey Bogart");
+    cout << dict.find("Humphrey Bogart") << endl;
+    cout << "Here 7" << endl;
     return 0;
 }
